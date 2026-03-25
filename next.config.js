2cd4498b3507a76a0f1342cfdf7635d46092d2de
TrevaOStech <tech@treva.in>
2026-03-25 20:14:27 +0530

    fix: add export step

--- next.config.js
+++ next.config.js
@@ -1,18 +1,5 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          { key: 'X-Content-Type-Options', value: 'nosniff' },
-          { key: 'X-Frame-Options', value: 'DENY' },
-          { key: 'X-XSS-Protection', value: '1; mode=block' },
-        ],
-      },
-    ];
-  },
-
-  // ✅ IMPORTANT (for Cloudflare)
   output: 'export',
 };
 
