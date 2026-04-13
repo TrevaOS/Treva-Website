39b8225c2d2d5272eb6814910bfc2f3e1e2cbbd9
Treva <tech@treva.in>
2026-04-13 11:51:24 +0000

    G$ Tags

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -3,23 +3,10 @@ import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import CRMNotification from '../components/CRMNotification';
 import { AnimatePresence, motion } from 'framer-motion';
-import Script from 'next/script';
 
 export default function App({ Component, pageProps, router }) {
   return (
     <>
-      <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-1Y78DKELJP"
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-1Y78DKELJP');
-        `}
-      </Script>
       <Navbar />
       <AnimatePresence mode="wait">
         <motion.div
