c5bfd460ab5aa98c899bd593d850c0d8e00829bc
Treva <tech@treva.in>
2026-04-29 08:09:16 +0000

    google tags

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -3,10 +3,45 @@ import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import CRMNotification from '../components/CRMNotification';
 import { AnimatePresence, motion } from 'framer-motion';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import Script from 'next/script';
+
+const GA_ID = 'G-1Y78DKELJP';
 
 export default function App({ Component, pageProps, router }) {
+  const nextRouter = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      if (typeof window.gtag === 'function') {
+        window.gtag('config', GA_ID, { page_path: url });
+      }
+    };
+
+    nextRouter.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      nextRouter.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [nextRouter.events]);
+
   return (
     <>
+      {/* Google Analytics */}
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          window.gtag = gtag;
+          gtag('js', new Date());
+          gtag('config', '${GA_ID}');
+        `}
+      </Script>
+
       <Navbar />
       <AnimatePresence mode="wait">
         <motion.div
--- pages/_document.jsx
+++ pages/_document.jsx
@@ -3,23 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        {/* Google tag (gtag.js) */}
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-1Y78DKELJP"
-        />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-1Y78DKELJP');
-            `,
-          }}
-        />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
